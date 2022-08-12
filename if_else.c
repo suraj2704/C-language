43398445a4928f1022a50272bd5a5e8d308d76c1
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-08-12 16:15:42 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+  printf("enter the number");
+  scanf("%d",&a);
+
+  if(a%2==0){
+    printf("it is even");
+    }
+
+ else {
+    printf("it is odd");
+    }
+  
+    return 0;
+}
+  
