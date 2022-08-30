089bf74d1da521b5e0dff1fa747ed819699dbb4c
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-08-30 21:51:36 +0530

    Create number_in_reverse_order.c

---
+++ number_in_reverse_order.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  for(int i=n;i;i--){
+    printf("the number is %d\n",i);
+  }
+    return 0;
+}
+  
