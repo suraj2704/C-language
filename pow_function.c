a1d69b01399fec29a55b30a0f511ce5d38d3eb1d
Suraj saini <107113880+suraj2704@users.noreply.github.com>
2022-09-22 20:29:04 +0530

    Create pow_function.c

---
+++ pow_function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int side;
+  printf("enter the value of side\n");
+  scanf("%d",&side);
+  printf("the value of side is %f",pow(side,2));
+    return 0;
+}
+  
